4edef8cef7ca5fae8ddf4b801d6e8343e42a8998
parammbhs <paramjitpatel23@lpu.in>
2025-06-23 17:03:37 +0530

    Add initial implementation of class1.cpp demonstrating pointers and references
    - Introduced a main function that showcases the use of pointers and references in C++.
    - Initialized an integer variable and demonstrated pointer manipulation.
    - Created a reference to another integer variable and modified its value.
    - Included examples of character pointers and their addresses.

---
+++ class1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x =10;
+    cout << "x = " << x << endl;
+    cout <<"&x = " << &x << endl;
+    int *p = &x;
+    cout << "p = " << p << endl;
+    *p += 10; 
+    cout << "*p = " << *p << endl;
+
+    int w = 10;
+    int &q = w;
+    cout << "w = " << w << endl;
+    cout << "q = " << q << endl;
+    q++;
+    cout << "q = " << q << endl;
+
+    char c = 'A';
+    char *pc = &c;
+    cout << "c = " << c << endl;
+    
+    cout << "pc without = " << &pc << endl;
+
+    int n =10;
+    n= n++;// This line has no effect on n as n++ returns the value before incrementing.
+    cout << "n = " << n << endl; // n remains 10
+    
+    
+}
